4ec92b7711cf046ec6fb5184aa616e085e30c7aa
Jojo Java <91952312+CodewithJoel@users.noreply.github.com>
2023-11-14 21:29:48 +0800

    Add files via upload

---
+++ people.js
@@ -0,0 +1,17 @@
+const people = [
+    'yoshi',
+    'jisoo',
+    'chaeyo',
+    'spider', 
+];
+
+const age = [
+    '21',
+    '25',
+    '19',
+    '39', 
+];
+
+module.exports = {
+    people, age
+};
